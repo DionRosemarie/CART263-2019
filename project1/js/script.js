457e85152359168a04d4967f98ff532ca8ab383d
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-02-05 23:45:40 -0500

    P1: Commenting my work

--- project1/js/script.js
+++ project1/js/script.js
@@ -6,9 +6,8 @@ Rose-Marie Dion
 ******************/
 
 
-
+// varibles for the game to work
 let $ant;
-
 let $leaf;
 let swap;
 let $wind;
@@ -17,37 +16,45 @@ let numLeaf = 6;
 let shakeLeaf = 500;
 let leafSound = new Audio("assets/sounds/leaves.mp3");
 
+// setup of the document
 $(document).ready(function() {
+  //setting all the variables
   $ant = $('#ant');
   $wind = $('#wind');
   $ant = $('#ant');
   $leaf = $('.leaf');
   $text = $('.text');
 
+  //the swap function is false otherwise it will swap when loading the page
   console.log($leaf);
   swap = false;
-
+  // hide the variables i don't need at the beginning
   $($wind).hide();
   $text.hide();
-
+  // on mouseover, the leaf that is draggable is going to shake
   $leaf.on("mouseover", leafShake);
 
+  // on click, the text of what to do will appear
   $leaf.click(function() {
-  $text.show();
-});
+    $text.show();
+  });
 
+  // draggable function
   $leaf.draggable({
+    // only starts when the leaf is dragged
     start: function(event, ui) {
       console.log('start');
       swap = true;
       setInterval(swapImages, 200);
     },
+    // stopped when the leaf is not dragged
     stop: function(event, ui) {
       swap = false;
     }
 
   });
 
+  // droppable function for the ant
   $($ant).droppable({
     drop: leafDropped,
 
@@ -56,8 +63,9 @@ $(document).ready(function() {
 
 });
 
+// functin to make the leaf shake when mouseover
 function leafShake() {
-   setInterval(shakeLeaf, $(this).effect("shake"));
+  setInterval(shakeLeaf, $(this).effect("shake"));
   leafSound.play();
 }
 
@@ -89,26 +97,31 @@ function leafDropped(event, ui, ) {
 
   }, function() {
     console.log("remove");
+    // if the object is dragged on the droppable object, remove it
     $(ui.draggable).remove();
-
+    // seeing how much leaves are gone
     numLeaf -= 1;
     console.log(numLeaf);
+    // make a new leaf appear when it is remove
     newLeaf();
-    leaf
   });
 
+  // make appear and disappear the element i want in my game/composition
   $($wind).show();
   $($wind).fadeOut(500);
   $text.fadeOut(500);
 }
-
+// new leaf function when it is remove
 function newLeaf() {
-
+  // When the leaf is remove, a new leaf appears by calling a new image
   $('body').append('<div class="leaf"><img src="./assets/images/leaf.png" id="leaf2" width="100" height="100"></div>');
   $leaf = $('.leaf');
 
+  // make the new leaf shake
   $leaf.on("mouseover", leafShake);
 
+
+  // make the new leaf draggable
   $leaf.draggable({
     start: function(event, ui) {
       console.log('start');
