a0f69465d0f202e54acaec5988db2a9d9263190a
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-03-05 10:36:02 -0500

    P2: Created different arrays for the different words to choose
    This is a test for the game to see if it works. I'm going to change everything once I know it works

--- project 2/js/script.js
+++ project 2/js/script.js
@@ -9,6 +9,63 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let name = [
+  "Jeremy",
+  "Alicia",
+  "Matthew",
+  "Sophia",
+  "Xavier",
+  "Lea",
+  "Samuel",
+  "Constance",
+  "Joseph",
+  "Catherine",
+  "Fiona"
+];
+
+let animal = [
+  "snake",
+  "elephant",
+  "crocodile",
+  "ladybug",
+  "spider",
+  "goldfish",
+  "sloth",
+  "monkey",
+  "cow",
+  "giraffe",
+  "hippopotamus",
+];
+
+let action = [
+  "cries",
+  "laughes",
+  "screams",
+  "complains",
+  "runs",
+  "falls",
+  "yells",
+  "signs"
+];
+
+let moment = [
+  "in the morning",
+  "at night",
+  "during the break",
+  "in the afternoon",
+  "before noon"
+];
+
+let place = [
+  "in the shower",
+  "at school",
+  "at work",
+  "at the party",
+  "at the bar",
+  "at the clinic",
+  "at the kitchen counter",
+  "at the movie theater"
+];
 
 // Get setup!
 $(document).ready(setup);
@@ -24,5 +81,4 @@ function setup() {
 
 function startGame() {
   $('#click-to-start').remove();
-
 }
