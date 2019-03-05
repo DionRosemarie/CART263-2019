d2da3add3357691a9189f58336e557df35f2a30f
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-03-05 10:21:38 -0500

    P2: Starting the game

--- project 2/js/script.js
+++ project 2/js/script.js
@@ -2,21 +2,16 @@
 
 /*****************
 
-Title of Project
-Author Name
+Once upon a time the internet
+Rose-Marie Dion
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
 
-// preload()
-//
-// Description of preload
-
-function preload() {
-
-}
+// Get setup!
+$(document).ready(setup);
 
 
 // setup()
@@ -24,14 +19,10 @@ function preload() {
 // Description of setup
 
 function setup() {
-
+  $('#click-to-start').on('click', startGame);
 }
 
-
-// draw()
-//
-// Description of draw()
-
-function draw() {
+function startGame() {
+  $('#click-to-start').remove();
 
 }
