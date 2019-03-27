600b421434bf4a2efd575f856182689d668d5e42
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-03-26 23:05:09 -0400

    Ex7: starts playing only once

--- assignment7/js/script.js
+++ assignment7/js/script.js
@@ -40,6 +40,7 @@ let pattern = ['x','*','xo*',' ','x','x','xo','*'];
 // Which beat of the pattern we're at right now
 let patternIndex = 0;
 
+let beatStarted = false;
 // setup()
 //
 // Creat canvas, set up the synth and sound files.
@@ -85,10 +86,12 @@ function setup() {
 // Using this to start the note and drum sequences to get around
 // user interaction (and to give the files time to load)
 function mousePressed() {
-  // Start an interval for the notes
-  setInterval(playNote,NOTE_TEMPO);
-  // Start an interval for the drums
-  setInterval(playDrum,DRUM_TEMPO);
+  if (beatStarted === false) {
+    setInterval(playNote,200);
+    setInterval(playDrum,100);
+    beatStarted = true;
+  }
+
 }
 
 // playNote
