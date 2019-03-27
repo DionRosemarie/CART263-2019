a1fa99d421d0682452535bbff0bb7890400f6357
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-03-26 22:40:08 -0400

    Ex7: starting assignment 7

--- project3/js/script.js
+++ project3/js/script.js
@@ -9,3 +9,21 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let size = 100;
+let pattern;
+$(document).ready(setup)
+function setup() {
+  // set up the base pattern
+  pattern = Trianglify({
+    height: window.innerHeight,
+    width: window.innerWidth,
+    cell_size: size})
+
+  // canvas
+  document.body.appendChild(pattern.canvas())
+}
+
+$('body').on('click', function(){
+  console.log('click');
+ pattern.recalculate({variance:5})
+})
